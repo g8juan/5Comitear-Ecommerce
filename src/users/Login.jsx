91b68e3b04c5f2a607641f5960055a993e15f600
Bartolome <bartolome@deltorto.com>
2020-11-05 17:43:36 -0300

    commit

--- src/users/Login.jsx
+++ src/users/Login.jsx
@@ -8,13 +8,11 @@ import Checkbox from "@material-ui/core/Checkbox";
 import Grid from "@material-ui/core/Grid";
 import Box from "@material-ui/core/Box";
 
-mport Typography from "@material-ui/core/Typography";
+import Typography from "@material-ui/core/Typography";
 import { makeStyles } from "@material-ui/core/styles";
 import Container from "@material-ui/core/Container";
 import { Link } from "react-router-dom";
 import Modal from "@material-ui/core/Modal";
-
-
 
 function Copyright() {
   return (
