ea0bc43fd0a80e5e1a3f8779b8362b39d1ce96a5
ManuelBorches <manuelborches@icloud.com>
2020-11-11 12:42:56 -0300

    vista de admin

--- src/navbar/NavigationBar.jsx
+++ src/navbar/NavigationBar.jsx
@@ -39,6 +39,7 @@ export default function NavigationBar({handleSubmit, user, logOut}) {
       <Form inline="true" onSubmit={handleSubmit}>
         <FormControl type="text" placeholder="Search" className="mr-sm-2" />
         <Nav className="mr-auto">
+          {user.userType === "1" ? 
           <Button as={Link} to="/cart" className="m-1">
             My Cart {"  "}
             <svg
@@ -54,17 +55,19 @@ export default function NavigationBar({handleSubmit, user, logOut}) {
                 d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l1.313 7h8.17l1.313-7H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm7 0a1 1 0 1 0 0 2 1 1 0 0 0 0-2z"
               />
             </svg>
-          </Button>
+          </Button> 
+          : null}
           {!user.id ? (
             <>
               <Button inline="true" as={Link} to="/register" className="m-1">Sign up{"  "}</Button>
               <Button as={Link} to="/login" className="m-1"> Log in </Button>
             </>
           ) : (
-              <>
+              <>{user.userType === "1" ? 
                 <Button as={Link} to="/orders" className="m-1">My purchases {"  "}</Button>
-                <Button onClick={logOut} as={Link} to="/" className="m-1">Log Out</Button>
+              : null}
                 <p style={{margin: '0.5rem', alignSelf: 'center', color: 'white'}}>Bienvenido {user.firstName}!</p>
+                <Button onClick={logOut} as={Link} to="/" className="m-1">Log Out</Button>
               </>
             )}
         </Nav>
