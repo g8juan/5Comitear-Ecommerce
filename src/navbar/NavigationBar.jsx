a6ba5665866a5afc69463993ad8bfa568d60dc53
ManuelBorches <manuelborches@icloud.com>
2020-11-13 11:29:24 -0300

    feature cart view

--- src/navbar/NavigationBar.jsx
+++ src/navbar/NavigationBar.jsx
@@ -48,22 +48,6 @@ export default function NavigationBar({ handleSubmit, user, logOut }) {
       <Form inline="true" onSubmit={handleSubmit}>
         <FormControl type="text" placeholder="Search" className="mr-sm-2" />
         <Nav className="mr-auto">
-          <Button as={Link} to="/cart" className="m-1" variant="secondary">
-            {user.userType ? `My Cart ` : `Cart `}
-            <svg
-              width="1em"
-              height="1em"
-              viewBox="0 0 16 16"
-              className="bi bi-cart"
-              fill="currentColor"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                fillRule="evenodd"
-                d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l1.313 7h8.17l1.313-7H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm7 0a1 1 0 1 0 0 2 1 1 0 0 0 0-2z"
-              />
-            </svg>
-          </Button>
 
           {!user.id ? (
             <>
@@ -72,9 +56,28 @@ export default function NavigationBar({ handleSubmit, user, logOut }) {
             </>
           ) : (
               <>{user.userType === "1" ?
+              <div> 
+              <Button as={Link} to="/cart" className="m-1" variant="secondary">
+              {user.userType ? `My Cart ` : `Cart `}
+              <svg
+                width="1em"
+                height="1em"
+                viewBox="0 0 16 16"
+                className="bi bi-cart"
+                fill="currentColor"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l1.313 7h8.17l1.313-7H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm7 0a1 1 0 1 0 0 2 1 1 0 0 0 0-2z"
+                />
+              </svg>
+              </Button>
+
                 <Button as={Link} to="/orders" className="m-1" variant="secondary">
                   My purchases {"  "}
                 </Button>
+                </div>
                 : null}
                 <p style={{ margin: '0.5rem', alignSelf: 'center', color: 'white' }}>
                   Welcome {user.firstName}!
